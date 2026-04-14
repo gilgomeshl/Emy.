e728ebfbffd40e95b608dfedb2cc422726edb261
gilgomeshl <gilgomeshl788@gmail.com>
2026-04-13 21:08:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 function mostrarSecao(id) {
   const secoes = document.querySelectorAll('.secao');
-  const botoes = document.querySelectorAll('nav button');
-
-  secoes.forEach(secao => secao.classList.remove('ativa'));
+  secoes.forEach(secao => {
+    secao.classList.remove('ativa');
+  });
   document.getElementById(id).classList.add('ativa');
-
-  botoes.forEach(botao => botao.classList.remove('active'));
-  event.target.classList.add('active');
 }
 
 function mostrarMensagem() {
