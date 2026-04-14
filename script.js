bfe0f0a0a315b9829d648f3eb9d6a58dbf521136
gilgomeshl <gilgomeshl788@gmail.com>
2026-04-13 21:36:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Controle das seções
 function mostrarSecao(id) {
   const secoes = document.querySelectorAll('.secao');
   const botoes = document.querySelectorAll('nav button');
@@ -11,4 +12,46 @@ function mostrarSecao(id) {
 
 function mostrarMensagem() {
   document.getElementById('mensagem').style.display = 'block';
+}
+
+// Galeria com lightbox + carrossel + slideshow
+let fotos = [
+  "images/foto3.jpg",
+  "images/foto4.jpg",
+  "images/foto5.jpg"
+];
+let fotoAtual = 0;
+let slideshowInterval;
+
+function abrirLightbox(index) {
+  fotoAtual = index;
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'flex';
+  atualizarFoto();
+
+  // Inicia slideshow automático (troca a cada 4 segundos)
+  slideshowInterval = setInterval(() => {
+    mudarFoto(1);
+  }, 4000);
+}
+
+function fecharLightbox() {
+  document.getElementById('lightbox').style.display = 'none';
+  clearInterval(slideshowInterval); // Para o slideshow ao fechar
+}
+
+function mudarFoto(direcao) {
+  fotoAtual += direcao;
+  if (fotoAtual < 0) fotoAtual = fotos.length - 1;
+  if (fotoAtual >= fotos.length) fotoAtual = 0;
+  atualizarFoto();
+}
+
+function atualizarFoto() {
+  const lightboxImg = document.getElementById('lightbox-img');
+  lightboxImg.classList.remove('mostrar');
+  setTimeout(() => {
+    lightboxImg.src = fotos[fotoAtual];
+    lightboxImg.classList.add('mostrar');
+  }, 100);
 }
